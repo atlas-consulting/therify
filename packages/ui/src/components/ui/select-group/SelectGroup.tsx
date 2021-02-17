95510504e83def281952150ed38ca5524f934179
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2021-02-16 19:02:40 -0600

    test: Select and selectGroup tests

--- packages/ui/src/components/ui/select-group/SelectGroup.tsx
+++ packages/ui/src/components/ui/select-group/SelectGroup.tsx
@@ -18,7 +18,7 @@ export const SelectGroup = ({ configs }: SelectGroupProps) => {
     return (
         <Box>
             {configs.map(({ id, options, name, onChange, selectedValue }, i) => (
-                <Box key={id} display="inline-block" marginRight={i === configs.length - 1 ? 0 : 0.5}>
+                <Box id={id} key={id} display="inline-block" marginRight={i === configs.length - 1 ? 0 : 0.5}>
                     <Select name={name} options={options} selectedValue={selectedValue} onChange={onChange} />
                 </Box>
             ))}
