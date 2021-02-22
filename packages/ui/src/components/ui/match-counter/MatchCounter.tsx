458c3528a50d97fe52f5f2647e5aeb0dffe7289c
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2021-02-22 01:24:13 -0600

    refactor: move match quality sorting to @admin

--- packages/ui/src/components/ui/match-counter/MatchCounter.tsx
+++ packages/ui/src/components/ui/match-counter/MatchCounter.tsx
@@ -3,42 +3,30 @@ import { MatchTypes } from '@therify/types';
 import { Text } from '../../core/typography';
 import React from 'react';
 export type MatchCounterProps = {
-    matches: MatchTypes.Match[];
+    good: MatchTypes.Match[];
+    warnings: MatchTypes.Match[];
+    incompatibilities: MatchTypes.Match[];
 };
-// TODO: determine how to check matches
-const hasIssues = (match: MatchTypes.Match) => !!match.hasIssues;
-const isIncompatible = (match: MatchTypes.Match) => !!match.isIncompatible;
-const separateMatchQualities = (matches: MatchTypes.Match[]) => {
-    return matches.reduce(
-        (matchAcc, match) => {
-            if (isIncompatible(match)) {
-                matchAcc.incompatibilies.push(match);
-            } else if (hasIssues(match)) {
-                matchAcc.warnings.push(match);
-            } else {
-                matchAcc.rankings.push(match);
-            }
-            return matchAcc;
-        },
-        { rankings: [], warnings: [], incompatibilies: [] },
-    );
-};
-export const MatchCounter = ({ matches }: MatchCounterProps) => {
+
+export const MatchCounter = ({ good, warnings, incompatibilities }: MatchCounterProps) => {
     const theme = useTheme();
-    const { rankings, warnings, incompatibilies } = separateMatchQualities(matches);
     return (
         <Box>
             <Text data-testid="match-counter-rankings" display="inline" style={{ marginRight: theme.spacing(2) }}>
-                <span style={{ color: theme.palette.success.main }}>{rankings.length}</span> ranking
-                {rankings.length === 1 ? '' : 's'}
+                <span style={{ color: theme.palette.success.main }}>{good.length}</span> ranking
+                {good.length === 1 ? '' : 's'}
             </Text>
             <Text data-testid="match-counter-warnings" display="inline" style={{ marginRight: theme.spacing(2) }}>
                 <span style={{ color: theme.palette.warning.main }}>{warnings.length}</span> warning
                 {warnings.length === 1 ? '' : 's'}
             </Text>
-            <Text data-testid="match-counter-incompatibilies" display="inline" style={{ marginRight: theme.spacing(2) }}>
-                <span style={{ color: theme.palette.error.main }}>{incompatibilies.length}</span> incompatibilit
-                {incompatibilies.length === 1 ? 'y' : 'ies'}
+            <Text
+                data-testid="match-counter-incompatibilies"
+                display="inline"
+                style={{ marginRight: theme.spacing(2) }}
+            >
+                <span style={{ color: theme.palette.error.main }}>{incompatibilities.length}</span> incompatibilit
+                {incompatibilities.length === 1 ? 'y' : 'ies'}
             </Text>
         </Box>
     );
