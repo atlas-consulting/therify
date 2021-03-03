b7da55a802e0eedb1c3a48fdd81610349c7d1ead
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2021-03-03 00:17:43 -0600

    feat: add hook to use MatchesApi

--- packages/admin/src/api/MatchesApi.ts
+++ packages/admin/src/api/MatchesApi.ts
@@ -4,14 +4,48 @@ import { mockModelResultsList } from './mocks/RankingResult';
 export type getMatchesOptions = {
     token: string;
 };
+export type createMatchOptions = {
+    patientId: string;
+    providerId: string;
+};
 
 const MatchesApiCreator = () => {
     const getMatches = async (options: getMatchesOptions) => {
         return await new Promise<MatchTypes.Match[]>((resolve) =>
-            setTimeout(() => resolve(mockModelResultsList), 2000),
+            setTimeout(() => {
+                console.log(`%cGetting Matches`, 'color: green');
+                resolve(mockModelResultsList);
+            }, 2000),
         );
     };
-    return { getMatches };
+    const createMatch = async ({ patientId, providerId }: createMatchOptions) => {
+        return await new Promise<void>((resolve) =>
+            setTimeout(() => {
+                console.log(
+                    `%cCreating Match for patient '${patientId}' and provider '${providerId}'...`,
+                    'color: green',
+                );
+                resolve();
+            }, 2000),
+        );
+    };
+    const approveMatch = async (matchId: string) => {
+        return await new Promise<void>((resolve) =>
+            setTimeout(() => {
+                console.log(`%cApproving Match: ${matchId}`, 'color: green');
+                resolve();
+            }, 2000),
+        );
+    };
+    const denyMatch = async (matchId: string, reason?: string) => {
+        return await new Promise<void>((resolve) =>
+            setTimeout(() => {
+                console.log(`%cDenying Match: ${matchId}`, 'color: red');
+                resolve();
+            }, 2000),
+        );
+    };
+    return { getMatches, createMatch, approveMatch, denyMatch };
 };
 
 export const MatchesApi = MatchesApiCreator();
