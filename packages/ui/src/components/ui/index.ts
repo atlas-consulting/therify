a56991b2137f52226357c48f8b829645aa40481a
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2021-03-06 17:21:56 -0600

    chore: export provider ranking from ui

--- packages/ui/src/components/ui/index.ts
+++ packages/ui/src/components/ui/index.ts
@@ -2,5 +2,6 @@ export * from './select-group';
 export * from './match-counter';
 export * from './matches-card';
 export * from './preferences-grid';
+export * from './provider-ranking';
 export * from './search-bar';
 export * from './split-button';
