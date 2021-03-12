3efd9433883a3d65266ebd9ffb81ce087c2eb839
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2021-03-11 22:42:15 -0600

    chore: add name selectors

--- packages/admin/src/store/selectors/userSelector.ts
+++ packages/admin/src/store/selectors/userSelector.ts
@@ -4,3 +4,9 @@ type Store = { user: UserStore };
 export const getUserState = ({ user }: Store) => user;
 
 export const getUserToken = ({ user }: Store) => user.token;
+
+export const getUserFirstName = ({ user }: Store) => user.firstName;
+
+export const getUserLastName = ({ user }: Store) => user.lastName;
+
+export const getUserName = ({ user: { firstName, lastName } }: Store) => `${firstName} ${lastName}`;
