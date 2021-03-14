9c5f66c4dd847bbb6b528ff722e9e00f72b5198a
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2021-03-14 12:50:50 -0500

    feat: create more deterministic return type

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -1,18 +1,52 @@
 import { S3 } from 'aws-sdk';
 import { Intake } from '@therify/types';
 import { getConfig } from './configuration';
+
+const PERSIST_SUBMISSION_SUCCESS = 'SUCCESS';
+const PERSIST_SUBMISSION_FAILURE = 'FAILURE';
+const PERSIST_SUBMISSION_RESULT_TYPES = [PERSIST_SUBMISSION_SUCCESS, PERSIST_SUBMISSION_FAILURE] as const;
+export type PersistSubmissionResultType = typeof PERSIST_SUBMISSION_RESULT_TYPES[number];
+interface PersistSubmissionSuccess {
+    type: typeof PERSIST_SUBMISSION_SUCCESS;
+    bucketId: string;
+    key: string;
+}
+
+interface PersistSubmissionError {
+    type: typeof PERSIST_SUBMISSION_FAILURE;
+    rawMessage: string | Record<string, unknown>;
+}
+
+export type PersistSubmissionResult = PersistSubmissionSuccess | PersistSubmissionError;
+
 /**
  *
  */
-export async function persistSubmission(subject: Intake.IntakeSubject, submission: Intake.IntakeSubmission) {
+export async function persistSubmission(
+    subject: Intake.IntakeSubject,
+    submission: Intake.IntakeSubmission,
+): Promise<PersistSubmissionResult> {
     const s3 = new S3();
     const config = getConfig();
-    const result = await s3
-        .putObject({
-            Bucket: config.CLIENT_INTAKE_BUCKET_NAME,
-            Key: `${submission.formID}/${submission.submissionID}`,
-            Body: submission.rawRequest,
-        })
-        .promise();
-    return result;
+    const bucketId: string = config.CLIENT_INTAKE_BUCKET_NAME;
+    const key = `${submission.formID}/${submission.submissionID}`;
+    try {
+        await s3
+            .putObject({
+                Bucket: bucketId,
+                Key: key,
+                Body: submission.rawRequest,
+            })
+            .promise();
+        return {
+            type: 'SUCCESS',
+            bucketId,
+            key,
+        };
+    } catch (persistError) {
+        return {
+            type: 'FAILURE',
+            rawMessage: submission.rawRequest,
+        };
+    }
 }
