34c493c34a1ea9141196140b4bf3a3f24d99bd43
Jessie Wooten <JessieWootenVisual@gmail.com>
2021-03-23 17:39:27 -0500

    Feat(admin-ui)/amplify init (#16)
    * feat: add amplify
    
    * feat: setup Amplify
    
    Co-authored-by: Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -2,11 +2,51 @@ import React from 'react';
 import { therafyTheme } from '@therify/ui';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import { Provider as StoreProvider } from 'react-redux';
+import Amplify, { Hub, Logger } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import awsExports from './aws-exports';
 import store from './store';
 import './App.css';
 import { Router } from './routes';
 
+const withEnvironmentVars = (awsExports: Record<string, any>) => ({
+    ...awsExports,
+    aws_cognito_region: process.env.REACT_APP_AWS_REGION,
+    aws_user_pools_id: process.env.REACT_APP_COGNITO_POOLS_ID,
+    aws_user_pools_web_client_id: process.env.REACT_APP_COGNITO_CLIENT_ID,
+});
+console.log(withEnvironmentVars(awsExports));
+Amplify.configure(withEnvironmentVars(awsExports));
+
 function App() {
+    const logger = new Logger('My-Logger');
+    const listener = (data: any) => {
+        switch (data.payload.event) {
+            case 'signIn':
+                console.log(data);
+                logger.info('user signed in');
+                break;
+            case 'signUp':
+                logger.info('user signed up');
+                break;
+            case 'signOut':
+                logger.info('user signed out');
+                break;
+            case 'signIn_failure':
+                logger.error('user sign in failed');
+                break;
+            case 'tokenRefresh':
+                logger.info('token refresh succeeded');
+                break;
+            case 'tokenRefresh_failure':
+                logger.error('token refresh failed');
+                break;
+            case 'configured':
+                logger.info('the Auth module is configured');
+        }
+    };
+
+    Hub.listen('auth', listener);
     return (
         <ThemeProvider theme={therafyTheme}>
             <StoreProvider store={store}>
@@ -17,4 +57,4 @@ function App() {
     );
 }
 
-export default App;
+export default withAuthenticator(App);
