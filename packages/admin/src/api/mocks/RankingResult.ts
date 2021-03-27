8360daa209fc0698fa86d4f8f5c66b435a1d4fda
Jessie Wooten <JessieWootenVisual@gmail.com>
2021-03-27 14:14:15 -0500

    ci: use yarn over npm

--- packages/admin/src/api/mocks/RankingResult.ts
+++ packages/admin/src/api/mocks/RankingResult.ts
@@ -36,7 +36,7 @@ export const mockModelResult: MatchTypes.Match = {
                 state: 'TN',
                 acceptedNetworks: ['Cigna'],
                 gender: 'male',
-                race: 'No ',
+                race: 'No',
                 specialty: 'Stress',
             },
         },
